c30135cbf26593317bcb6fc18d2a33b09ed03c3f
adeolaadedeji <delaji@yahoo.com>
2022-05-12 14:22:22 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was created by{" "}
-        <a href="http://localhost:3000/" target="_blank" rel="noreferrer">
-          Adeola Adedeji
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/adeolaadedeji/react-weather-application"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was created by{" "}
+          <a href="http://localhost:3000/" target="_blank" rel="noreferrer">
+            Adeola Adedeji
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/adeolaadedeji/react-weather-application"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
