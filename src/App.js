e951700a81c7a3314cfe308374f64c9fccbb010b
adeolaadedeji <delaji@yahoo.com>
2022-05-17 22:23:27 +0100

    Created a new component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Lagos" />
         <footer>
           This project was created by{" "}
           <a href="http://localhost:3000/" target="_blank" rel="noreferrer">
